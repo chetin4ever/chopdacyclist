712cb5118a5a171c1467e3c7eb030e7c4f9a0551
chetin4ever <hetan009@gmail.com>
2026-01-23 15:28:18 +0530

    repuplic day certificate changes made

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -57,19 +57,22 @@ const Certificate = () => {
 
   return (
     <div className='min-h-screen bg-gray-100'>
-      <nav className='flex items-center justify-between bg-green-900 text-white px-6 py-3 shadow-md'>
+      <nav
+        className='flex items-center justify-between
+                bg-gradient-to-r from-[#FF9933] via-white to-[#138808]
+                px-6 py-3 shadow-md'
+      >
         <div className='flex items-center gap-3'>
-          {/* Cycling icon */}
-          <Bike className='h-15 w-15 text-white' />
-
-          <h1 className='text-3xl font-bold'>Chopda Cyclist</h1>
+          <Bike className='h-8 w-8 text-[#000080]' />
+          <h1 className='text-3xl font-bold text-[#000080]'>Chopda Cyclist</h1>
         </div>
       </nav>
       {/* Input */}
       <div className='flex flex-col items-center mt-6'>
         <label
           htmlFor='name'
-          className='mb-2 text-lg font-semibold text-green-900'
+          className='block mb-2 text-sm font-semibold text-[#000080]
+             border-l-4 border-[#FF9933] pl-2'
         >
           Participant Name
         </label>
@@ -79,10 +82,13 @@ const Certificate = () => {
           placeholder='Enter Your name'
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className='w-80 px-4 py-3 border-2 border-green-700 rounded-xl shadow-sm
-               focus:outline-none focus:ring-2 focus:ring-yellow-400 
-               font-body text-lg text-gray-800 placeholder-gray-400
-               transition-all duration-200'
+          className='w-80 px-4 py-3 rounded-xl shadow-sm
+           border-2
+           border-t-[#FF9933] border-l-[#FF9933]
+           border-r-[#138808] border-b-[#138808]
+           focus:outline-none focus:ring-2 focus:ring-[#000080]
+           font-body text-lg text-[#000080] placeholder-gray-500
+           transition-all duration-200'
         />
       </div>
 
@@ -95,17 +101,19 @@ const Certificate = () => {
           >
             {/* Background certificate image */}
             <img
-              src='/chopdacyclistcer.png'
+              src='/republicdaycertificate.png'
               alt='certificate'
-              className='w-full h-full object-contain'
+              className='w-full h-full '
             />
 
             {/* Name Overlay */}
             {name && (
-              <div className='absolute top-[43%] left-1/2 -translate-x-1/2 whitespace-nowrap text-center px-2'>
+              <div className='absolute top-[57%] left-1/2 -translate-x-1/2 whitespace-nowrap text-center px-2'>
                 <h2
-                  className='text-base sm:text-xl md:text-2xl lg:text-4xl font-fancy'
-                  style={{ color: "#006400" }}
+                  className='text-base sm:text-xl md:text-2xl lg:text-4xl font-fancy font-semibold
+           bg-gradient-to-r from-[#FF9933] via-[#F5F5F5] to-[#138808]
+           bg-clip-text text-transparent'
+                  style={{ WebkitTextStroke: "0.6px rgba(0,0,0,0.35)" }}
                 >
                   {name}
                 </h2>
@@ -119,21 +127,24 @@ const Certificate = () => {
           <button
             onClick={downloadPNG}
             disabled={!name}
-            className='w-40 md:w-80 px-6 py-3 rounded-xl font-semibold shadow-md 
-               bg-gradient-to-r from-green-800 to-yellow-500 text-white
-               hover:from-green-700 hover:to-yellow-400 
-               disabled:opacity-50 disabled:cursor-not-allowed 
+            className='w-40 md:w-80 px-6 py-3 rounded-xl font-semibold shadow-md
+               bg-gradient-to-r from-[#FF9933] via-[#F5F5F5] to-[#138808]
+               text-[#000080]
+               hover:from-[#ff8c1a] hover:via-[#ffffff] hover:to-[#0f6f06]
+               disabled:opacity-50 disabled:cursor-not-allowed
                transition-all duration-300'
           >
             Download PNG
           </button>
+
           <button
             onClick={downloadPDF}
             disabled={!name}
-            className='w-40 md:w-80 px-6 py-3 rounded-xl font-semibold shadow-md 
-               bg-gradient-to-r from-green-800 to-yellow-500 text-white
-               hover:from-green-700 hover:to-yellow-400 
-               disabled:opacity-50 disabled:cursor-not-allowed 
+            className='w-40 md:w-80 px-6 py-3 rounded-xl font-semibold shadow-md
+               bg-gradient-to-r from-[#FF9933] via-[#F5F5F5] to-[#138808]
+               text-[#000080]
+               hover:from-[#ff8c1a] hover:via-[#ffffff] hover:to-[#0f6f06]
+               disabled:opacity-50 disabled:cursor-not-allowed
                transition-all duration-300'
           >
             Download PDF
